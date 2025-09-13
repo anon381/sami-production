cfbf833a07f39d876a96dbbf271a14bc2ea4bc94
Yabets Maregn <yabetsanon@gmail.com>
2025-09-13 21:22:57 +0900

    Comment out ServicePortfolio component in CameraWorkPage

--- app/services/camera-work/page.tsx
+++ app/services/camera-work/page.tsx
@@ -58,7 +58,7 @@ export default function CameraWorkPage() {
       <ServiceDetailHero {...serviceData} />
       <ServiceFeatures features={serviceData.features} />
       <ServiceProcess process={serviceData.process} />
-      <ServicePortfolio portfolio={serviceData.portfolio} />
+      {/* <ServicePortfolio portfolio={serviceData.portfolio} /> */}
     </main>
   )
 }
